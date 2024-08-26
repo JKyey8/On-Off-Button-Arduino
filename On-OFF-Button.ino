62b9d3358adb37e9fe2e8b70997a77b39e635d8a
Jacob K <73721102+JKyey8@users.noreply.github.com>
2024-08-25 20:51:48 -0700

    Add files via upload

--- On-OFF-Button.ino
+++ On-OFF-Button.ino
@@ -7,22 +7,38 @@ int LED3 = 11;
 void setup() {
   // put your setup code here, to run once:
   pinMode (12,OUTPUT);
-  pinMode(2,INPUT);
-  digitalWrite(12,HIGH);
+  pinMode(11,INPUT);
+  pinMode(5,OUTPUT);
+  digitalWrite(11,HIGH);
+  digitalWrite(5,HIGH);
 
 }
 void loop() {
   // put your main code here, to run repeatedly:
+if(digitalRead(2) == HIGH && digitalRead(11) == HIGH){
+for (int fadeValue = 255; fadeValue >= 0; fadeValue = fadeValue -= 5){
+analogWrite(11,fadeValue);
+analogWrite(5,fadeValue);
 
-  
-if(digitalRead(2) == HIGH && digitalRead(12) == HIGH) {
-digitalWrite(12,LOW);
+delay(30);
+};
+} else if (digitalRead(2) == HIGH && digitalRead(11) == LOW){
+ // digitalWrite(11,LOW);
+// digitalWrite(5,HIGH);
 
-} else if (digitalRead(2) == HIGH && digitalRead(12) == LOW)  {
-digitalWrite(12,HIGH);
+for (int fadeValue = 0; fadeValue <= 255; fadeValue = fadeValue += 5){
+analogWrite(11,fadeValue);
+analogWrite(5,fadeValue);
 
+delay(30);
+
+};
+// digitalWrite(11,HIGH);
+ // digitalWrite(5,LOW);
+
+}
 }
 
 
 
-}
+
